db47f8cb23cec43a09e8ff48dbddf75dcb35cc8d
rubenavaldez <rubenavaldez@yahoo.com>
2020-06-03 14:07:05 -0700

    beta test basic js with names

--- module1names.js
+++ module1names.js
@@ -210,3 +210,51 @@ const BasicDataStructuresModuleNames=[
     "Generate an Array of All Object Keys with Object.keys()",
     "Modify an Array Stored in an Object"
 ];
+
+const BasicAlgorithmScriptingModuleNames=[
+    "Convert Celsius to Fahrenheit",
+    "Reverse a String",
+    "Factorialize a Number",
+    "Find the Longest Word in a String",
+    "Return Largest Numbers in Arrays",
+    "Confirm the Ending",
+    "Repeat a String Repeat a String",
+    "Truncate a String",
+    "Finders Keepers",
+    "Boo who",
+    "Title Case a Sentence",
+    "Slice and Splice",
+    "Falsy Bouncer",
+    "Where do I Belong",
+    "Mutations",
+    "Chunky Monkey"
+];
+
+const ObjectOrientedProgrammingModuleNames=[
+    "Create a Basic JavaScript Object",
+    "Use Dot Notation to Access the Properties of an Object",
+    "Create a Method on an Object",
+    "Make Code More Reusable with the this Keyword",
+    "Define a Constructor Function",
+    "Use a Constructor to Create Objects",
+    "Extend Constructors to Receive Arguments",
+    "Verify an Object's Constructor with instanceof",
+    "Understand Own Properties",
+    "Use Prototype Properties to Reduce Duplicate Code",
+    "Iterate Over All Properties",
+    "Understand the Constructor Property",
+    "Change the Prototype to a New Object",
+    "Remember to Set the Constructor Property when Changing the Prototype",
+    "Understand Where an Object’s Prototype Comes From",
+    "Understand the Prototype Chain",
+    "Use Inheritance So You Don't Repeat Yourself",
+    "Inherit Behaviors from a Supertype",
+    "Set the Child's Prototype to an Instance of the Parent",
+    "Reset an Inherited Constructor Property",
+    "Add Methods After Inheritance",
+    "Override Inherited Methods",
+    "Use a Mixin to Add Common Behavior Between Unrelated Objects",
+    "Use Closure to Protect Properties Within an Object from Being Modified Externally",
+    "Understand the Immediately Invoked Function Expression (IIFE)",
+    "Use an IIFE to Create a Module"
+];
