67cfee0b3777eca7dd0f687cbcf9fd6becaa9f13
rubenavaldez <rubenavaldez@yahoo.com>
2020-06-03 11:23:57 -0700

    created module1Names.js

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import basicJavascriptModuleNames from "./module1names.js"
 let currentStudent = undefined;
 $(document).on("click", "#get-student", function () {
     
@@ -185,51 +186,3 @@ $(document).on("click", "#get-question", function () {
 
     }
 })
-
-let basicJavascriptModuleNames=[
-    "Comment Your JavaScript Code",
-    "Declare JavaScript Variables",
-    "Storing Values with the Assignment Operator",
-    "Initializing Variables with the Assignment Operator",
-    "Understanding Uninitialized Variables",
-    "Understanding Case Sensitivity in Variables",
-    "Add Two Numbers with JavaScript",
-    "Subtract One Number from Another with JavaScript",
-    "Multiply Two Numbers with JavaScript",
-    "Divide One Number by Another with JavaScript",
-    "Increment a Number with JavaScript",
-    "Decrement a Number with JavaScript",
-    "Create Decimal Numbers with JavaScript",
-    "Multiply Two Decimals with JavaScript",
-    "Divide One Decimal by Another with JavaScript",
-    "Finding a Remainder in JavaScript",
-    "Compound Assignment With Augmented Addition",
-    "Compound Assignment With Augmented Subtraction",
-    "Compound Assignment With Augmented Multiplication",
-    "Compound Assignment With Augmented Division",
-    "Declare String Variables",
-    "Escaping Literal Quotes in Strings",
-    "Quoting Strings with Single Quotes",
-    "Escape Sequences in Strings",
-    "Concatenating Strings with Plus Operator",
-    "Concatenating Strings with the Plus Equals Operator",
-    "Constructing Strings with Variables",
-    "Appending Variables to Strings",
-    "Find the Length of a String",
-    "Use Bracket Notation to Find the First Character in a String",
-    "Understand String Immutability",
-    "Use Bracket Notation to Find the Nth Character in a String",
-    "Use Bracket Notation to Find the Last Character in a String",
-    "Use Bracket Notation to Find the Nth-to-Last Character in a String",
-    "Word Blanks",
-    "Store Multiple Values in one Variable using JavaScript Arrays",
-    "Nest one Array within Another Array",
-    "Access Array Data with Indexes",
-    "Modify Array Data With Indexes",
-    "Access Multi-Dimensional Arrays With Indexes"
-
-
-
-
-
-];
