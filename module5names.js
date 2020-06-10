ec7cca8db159d001c4fae7452d79f9ca8123722f
rubenavaldez <rubenavaldez@yahoo.com>
2020-06-10 13:50:20 -0700

    added names for all submodules

---
+++ module5names.js
@@ -0,0 +1,45 @@
+const ManagingPackagesWithNPMModuleNames =[
+
+    "How to Use package.json, the Core of Any Node.js Project or npm Package",
+    "Add a Description to Your package.json",
+    "Add Keywords to Your package.json",
+    "Add a License to Your package.json",
+    "Add a Version to Your package.json",
+    "Expand Your Project with External Packages from npm",
+    "Manage npm Dependencies By Understanding Semantic Versioning",
+    "Use the Tilde-Character to Always Use the Latest Patch Version of a Dependency",
+    "Use the Caret-Character to Use the Latest Minor Version of a Dependency",
+    "Remove a Package from Your Dependencies"
+]
+
+const BasicNodeAndExpress =[
+
+    "Meet the Node console",
+    "Start a Working Express Server",
+    "Serve an HTML File",
+    "Serve Static Assets",
+    "Serve JSON on a Specific Route",
+    "Use the .env File",
+    "Implement a Root-Level Request Logger Middleware",
+    "Chain Middleware to Create a Time Server",
+    "Get Route Parameter Input from the Client",
+    "Get Query Parameter Input from the Client",
+    "Use body-parser to Parse POST Requests",
+    "Get Data from POST Requests"
+]
+
+const MongoDBAndMongoose =[
+
+    "Install and Set Up Mongoose",
+    "Create a Model",
+    "Create and Save a Record of a Model",
+    "Create Many Records with model.create()",
+    "Use model.find() to Search Your Database",
+    "Use model.findOne() to Return a Single Matching Document from Your Database",
+    "Use model.findById() to Search Your Database By _id",                         //might throw an error
+    "Perform Classic Updates by Running Find, Edit, then Save",
+    "Perform New Updates on a Document Using model.findOneAndUpdate()",
+    "Delete One Document Using model.findByIdAndRemove",
+    "Delete Many Documents with model.remove()",
+    "Chain Search Query Helpers to Narrow Search Results"
+]
