f3ee5c0e730ede7e5bec0faa9576b13052491123
rubenavaldez <rubenavaldez@yahoo.com>
2020-07-06 09:37:59 -0700

    student progress updated

--- students.js
+++ students.js
@@ -7,26 +7,26 @@ class Student {
     }
 }
 //Name, Current Module, Current Submodule
-const Augustine = new Student("Augustine", 2,8)
+const Augustine = new Student("Augustine", 3,6)
 const Bruner = new Student("Bruner", 2,8)
 const Carroll = new Student("Carroll", 2,7)
 const Cook = new Student("Cook", 2,7)
 const Dearman = new Student("Dearman", 2,8)
 const Dickerson = new Student("Dickerson", 4,2)
-const Frazier = new Student("Frazier", 2,8)
+const Frazier = new Student("Frazier", 3,3)
 const Hardin = new Student("Hardin", 2,8)
-const Hall = new Student("Hall",2, 7 )
-const James = new Student("James", 2,7)
+const Hall = new Student("Hall",2, 8)
+const James = new Student("James", 2,8)
 const Jones = new Student("Jones",2,7)
 const Knox = new Student("Knox", 2,7)
 const Ney = new Student("Ney",2,7)
-const Ramos = new Student("Ramos",2,8)
-const Simental = new Student("Simental",3,3)
+const Ramos = new Student("Ramos",3,2)
+const Simental = new Student("Simental",3,5)
 const Spurgeon = new Student("Spurgeon", 2,8)
 const Thomas = new Student("Thomas", 2 ,7)
 const Turman = new Student("Turman",2,6)
-const Wuori = new Student("Wuori",2,8)
-const Yates = new Student("Yates",2,8)
+const Wuori = new Student("Wuori",3,4)
+const Yates = new Student("Yates",3,5)
 
 let studentArray = [
     Thomas,
