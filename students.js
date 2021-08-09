3504c8df6795e5716d7a0f66b9ab05f3edc6230c
Ruben Valdez <rubenavaldez@yahoo.com>
2021-08-09 08:51:42 -0700

    added students from cohort 2

--- students.js
+++ students.js
@@ -7,46 +7,46 @@ class Student {
     }
 }
 //Name, Current Module, Current Submodule
-const Augustine = new Student("Augustine", 3,6)
-const Bruner = new Student("Bruner", 2,8)
-const Carroll = new Student("Carroll", 2,7)
-const Cook = new Student("Cook", 2,7)
-const Dearman = new Student("Dearman", 2,8)
-const Dickerson = new Student("Dickerson", 4,2)
-const Frazier = new Student("Frazier", 3,3)
-const Hardin = new Student("Hardin", 2,8)
-const Hall = new Student("Hall",2, 8)
-const James = new Student("James", 2,8)
-const Jones = new Student("Jones",2,7)
-const Knox = new Student("Knox", 2,7)
-const Ney = new Student("Ney",2,7)
-const Ramos = new Student("Ramos",6,3)
-const Simental = new Student("Simental",3,5)
-const Spurgeon = new Student("Spurgeon", 2,8)
-const Thomas = new Student("Thomas", 2 ,7)
-const Turman = new Student("Turman",2,6)
-const Wuori = new Student("Wuori",3,4)
-const Yates = new Student("Yates",3,5)
+const LCorwin = new Student("LCorwin", 2,1)
+const RCorwin = new Student("RCorwin", 2,1)
+const Duchene = new Student("Duchene", 2,1)
+const Galvez = new Student("Galvez", 2,1)
+const Grover = new Student("Grover", 2,1)
+const Hillebrand = new Student("Hillebrand", 2,1)
+const Jones = new Student("Jones", 2,1)
+const Lopez = new Student("Lopez", 2,1)
+const Parilla = new Student("Parilla",2, 8)
+const Park = new Student("Park", 2,1)
+const Porter = new Student("Porter",2,1)
+const Sanchez = new Student("Sanchez", 4,2)
+const Stumpf = new Student("Stumpf",4,2)
+const Walker = new Student("Walker",2,1)
+const Simental = new Student("Simental",6,3)
+const Willenzik = new Student("Willenzik", 2,8)
+const Thomas = new Student("Thomas", 2 ,1)
+const Mansfield = new Student("Mansfield",2,1)
+const Wuori = new Student("Wuori",6,3)
+const Schaffner = new Student("Schaffner",2,1)
 
 let studentArray = [
     Thomas,
-    Knox,
-    Turman,
-    James,
+    Sanchez,
+    Mansfield,
+    Park,
     Jones,
-    Dearman,
-    Ney,
-    Spurgeon,
-    Bruner,
-    Frazier,
-    Ramos,
-    Hardin,
-    Carroll,
-    Cook,
-    Hall,
-    Dickerson,
+    Grover,
+    Stumpf,
+    Willenzik,
+    RCorwin,
+    Porter,
+    Walker,
+    Lopez,
+    Duchene,
+    Galvez,
+    Parilla,
+    Hillebrand,
     Simental,
-    Yates,
-    Augustine,
+    Schaffner,
+    LCorwin,
     Wuori   
 ];
